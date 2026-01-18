ee0f4900c5c28c0308c18b5c14894e94fc19a2b4
Franz Rosete <frosete@myseneca.ca>
2026-01-17 19:58:52 -0500

    completed lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Franz Aldrin Rosete
+#Author ID: frosete 157712183
+#Date Created: 2026-01-15
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+print('blast off!')
